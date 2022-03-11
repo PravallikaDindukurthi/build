aaf00c00d198e1fb7d0bdd079686c201ea1ecb73
Pravallika Dindukurthi <dindukurthi.pravallika@gmail.com>
2022-03-11 16:40:31 +0530

    Added build limit cleanup controller and reconciler logic
    Signed-off-by: Raghav Bhatnagar <raghavbhatnagar96@gmail.com>

---
+++ pkg/reconciler/build_limit_cleanup/build_limit_cleanup.go
@@ -0,0 +1,105 @@
+// Copyright The Shipwright Contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package build_limit_cleanup
+
+import (
+	"context"
+	"sort"
+
+	build "github.com/shipwright-io/build/pkg/apis/build/v1alpha1"
+	"github.com/shipwright-io/build/pkg/config"
+	"github.com/shipwright-io/build/pkg/ctxlog"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+// ReconcileBuild reconciles a Build object
+type ReconcileBuild struct {
+	// This client, initialized using mgr.Client() above, is a split client
+	// that reads objects from the cache and writes to the apiserver
+	config                *config.Config
+	client                client.Client
+	scheme                *runtime.Scheme
+	setOwnerReferenceFunc setOwnerReferenceFunc
+}
+
+func NewReconciler(c *config.Config, mgr manager.Manager, ownerRef setOwnerReferenceFunc) reconcile.Reconciler {
+	return &ReconcileBuild{
+		config:                c,
+		client:                mgr.GetClient(),
+		scheme:                mgr.GetScheme(),
+		setOwnerReferenceFunc: ownerRef,
+	}
+}
+
+func (r *ReconcileBuild) Reconcile(ctx context.Context, request reconcile.Request) (reconcile.Result, error) {
+	// Set the ctx to be Background, as the top-level context for incoming requests.
+	ctx, cancel := context.WithTimeout(ctx, r.config.CtxTimeOut)
+	defer cancel()
+
+	ctxlog.Debug(ctx, "start reconciling Build-Limit", namespace, request.Namespace, name, request.Name)
+
+	b := &build.Build{}
+	err := r.client.Get(ctx, request.NamespacedName, b)
+	if err != nil && !apierrors.IsNotFound(err) {
+		return reconcile.Result{}, err
+	} else if apierrors.IsNotFound(err) {
+		ctxlog.Debug(ctx, "finish reconciling build-limit. build was not found", namespace, request.Namespace, name, request.Name)
+		return reconcile.Result{}, nil
+	}
+
+	// Check if limit is set. If so, get all corresponding BR, else, return.
+	if b.Spec.Retention != nil {
+
+		lbls := map[string]string{
+			build.LabelBuild: b.Name,
+		}
+		opts := client.ListOptions{
+			Namespace:     b.Namespace,
+			LabelSelector: labels.SelectorFromSet(lbls),
+		}
+		allBuildRuns := &build.BuildRunList{}
+		r.client.List(ctx, allBuildRuns, &opts)
+		var buildRunSucceeded []build.BuildRun
+		var buildRunFailed []build.BuildRun
+		if b.Spec.Retention.FailedLimit != nil {
+			for _, br := range allBuildRuns.Items {
+				if br.Status.GetCondition(build.Succeeded).Status == corev1.ConditionFalse {
+					buildRunFailed = append(buildRunFailed, br)
+				}
+			}
+
+			if len(buildRunFailed) > int(*b.Spec.Retention.FailedLimit) {
+				// Sort the buildRunFailed based on the Completion time
+				sort.Slice(buildRunFailed, func(i, j int) bool {
+					return buildRunFailed[i].Status.CompletionTime.Before(buildRunFailed[j].Status.CompletionTime)
+				})
+				//Delete buildruns
+
+			}
+		}
+
+		if b.Spec.Retention.SucceededLimit != nil {
+			for _, br := range allBuildRuns.Items {
+				if br.Status.GetCondition(build.Succeeded).Status == corev1.ConditionTrue {
+					buildRunSucceeded = append(buildRunSucceeded, br)
+				}
+			}
+			if len(buildRunSucceeded) > int(*b.Spec.Retention.SucceededLimit) {
+				sort.Slice(buildRunSucceeded, func(i, j int) bool {
+					return buildRunSucceeded[i].Status.CompletionTime.Before(buildRunSucceeded[j].Status.CompletionTime)
+				})
+			}
+		}
+
+	}
+
+	return reconcile.Result{}, nil
+}
---
+++ pkg/reconciler/build_limit_cleanup/controller.go
@@ -0,0 +1,91 @@
+// Copyright The Shipwright Contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package build_limit_cleanup
+
+import (
+	"context"
+	"reflect"
+
+	build "github.com/shipwright-io/build/pkg/apis/build/v1alpha1"
+	"github.com/shipwright-io/build/pkg/config"
+	"github.com/shipwright-io/build/pkg/ctxlog"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+const (
+	namespace string = "namespace"
+	name      string = "name"
+)
+
+type setOwnerReferenceFunc func(owner, object metav1.Object, scheme *runtime.Scheme) error
+
+// Add creates a new Build Controller and adds it to the Manager. The Manager will set fields on the Controller
+// and Start it when the Manager is Started.
+func Add(ctx context.Context, c *config.Config, mgr manager.Manager) error {
+	ctx = ctxlog.NewContext(ctx, "build-limit-cleanup-controller")
+	return add(ctx, mgr, NewReconciler(c, mgr, controllerutil.SetControllerReference), c.Controllers.Build.MaxConcurrentReconciles)
+}
+
+func add(ctx context.Context, mgr manager.Manager, r reconcile.Reconciler, maxConcurrentReconciles int) error {
+	// Create the controller options
+	options := controller.Options{
+		Reconciler: r,
+	}
+
+	if maxConcurrentReconciles > 0 {
+		options.MaxConcurrentReconciles = maxConcurrentReconciles
+	}
+
+	// Create a new controller
+	c, err := controller.New("build-limit-cleanup-controller", mgr, options)
+	if err != nil {
+		return err
+	}
+
+	pred := predicate.Funcs{
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			o := e.ObjectOld.(*build.Build)
+			n := e.ObjectNew.(*build.Build)
+
+			// Check if the Retention field is updated
+			oldRetention := o.Spec.Retention
+			newRetention := n.Spec.Retention
+			if !reflect.DeepEqual(oldRetention, newRetention) {
+				if oldRetention.FailedLimit != newRetention.FailedLimit || oldRetention.SucceededLimit != newRetention.SucceededLimit {
+					ctxlog.Debug(
+						ctx,
+						"updating predicated passed, retention fields were modified.",
+						namespace,
+						n.GetNamespace(),
+						name,
+						n.GetName(),
+					)
+					return true
+				}
+			}
+			return false
+		},
+		DeleteFunc: func(e event.DeleteEvent) bool {
+			// Never reconcile on deletion, there is nothing we have to do
+			return false
+		},
+	}
+
+	// Watch for changes to primary resource Build
+	if err = c.Watch(&source.Kind{Type: &build.Build{}}, &handler.EnqueueRequestForObject{}, pred); err != nil {
+		return err
+	}
+
+	return nil
+}
