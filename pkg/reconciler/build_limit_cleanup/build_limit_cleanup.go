d47409009b7990e79b1d69ab95efaa1db6df63cd
raghavbhatnagar <raghavbhatnagar@in.ibm.com>
2022-03-14 17:57:56 +0530

    Changes to build reconciler to get task runs

--- pkg/reconciler/build_limit_cleanup/build_limit_cleanup.go
+++ pkg/reconciler/build_limit_cleanup/build_limit_cleanup.go
@@ -6,15 +6,18 @@ package build_limit_cleanup
 
 import (
 	"context"
+	"fmt"
 	"sort"
 
 	build "github.com/shipwright-io/build/pkg/apis/build/v1alpha1"
 	"github.com/shipwright-io/build/pkg/config"
 	"github.com/shipwright-io/build/pkg/ctxlog"
+	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
@@ -73,16 +76,36 @@ func (r *ReconcileBuild) Reconcile(ctx context.Context, request reconcile.Reques
 			for _, br := range allBuildRuns.Items {
 				if br.Status.GetCondition(build.Succeeded).Status == corev1.ConditionFalse {
 					buildRunFailed = append(buildRunFailed, br)
+					// ctxlog.Debug(ctx, "Failed Build Run. BuildRun name: ", br.Name, namespace)
 				}
+
 			}
 
 			if len(buildRunFailed) > int(*b.Spec.Retention.FailedLimit) {
+
 				// Sort the buildRunFailed based on the Completion time
 				sort.Slice(buildRunFailed, func(i, j int) bool {
 					return buildRunFailed[i].Status.CompletionTime.Before(buildRunFailed[j].Status.CompletionTime)
 				})
-				//Delete buildruns
+				// Delete buildruns
 
+				failedLimit := *b.Spec.Retention.FailedLimit
+				lenBuildRun := len(buildRunFailed)
+
+				for i := 0; lenBuildRun > int(failedLimit); i += 1 {
+					lastTaskRun := &v1beta1.TaskRun{}
+					getTaskRunErr := r.client.Get(ctx, types.NamespacedName{Name: buildRunFailed[i].Name, Namespace: request.Namespace}, lastTaskRun)
+					if getTaskRunErr != nil {
+						ctxlog.Debug(ctx, "Error getting task run for - ", buildRunFailed[i].Name, err)
+					}
+					ctxlog.Debug(ctx, "Get task run result: ", lastTaskRun.Name, namespace)
+					lenBuildRun -= 1
+					// get task run
+					// delete the task run
+					// delete build run
+					// poll to see if they are deleted <-?
+
+				}
 			}
 		}
 
@@ -90,13 +113,41 @@ func (r *ReconcileBuild) Reconcile(ctx context.Context, request reconcile.Reques
 			for _, br := range allBuildRuns.Items {
 				if br.Status.GetCondition(build.Succeeded).Status == corev1.ConditionTrue {
 					buildRunSucceeded = append(buildRunSucceeded, br)
+					// ctxlog.Debug(ctx, "Succeeded Build Run. BuildRun name: ", br, namespace)
 				}
 			}
 			if len(buildRunSucceeded) > int(*b.Spec.Retention.SucceededLimit) {
 				sort.Slice(buildRunSucceeded, func(i, j int) bool {
 					return buildRunSucceeded[i].Status.CompletionTime.Before(buildRunSucceeded[j].Status.CompletionTime)
 				})
+
+				succeededLimit := *b.Spec.Retention.SucceededLimit
+				lenBuildRun := len(buildRunSucceeded)
+
+				for i := 0; lenBuildRun > int(succeededLimit); i += 1 {
+					lastTaskRun := &v1beta1.TaskRun{}
+					getTaskRunErr := r.client.Get(ctx, types.NamespacedName{Name: buildRunSucceeded[i].Name, Namespace: request.Namespace}, lastTaskRun)
+					if getTaskRunErr != nil {
+						// ctxlog.Debug(ctx, "Error getting task run for - ", buildRunSucceeded[i], err)
+						fmt.Println("Error getting task run for: ------")
+						fmt.Println(buildRunSucceeded[i].Name)
+					} else {
+						// ctxlog.Debug(ctx, "Get task run result: ", lastTaskRun.Name, namespace)
+						fmt.Println("Task run: -----")
+						fmt.Println(lastTaskRun)
+
+						lenBuildRun -= 1
+						// 	// get task run
+						// 	// delete the task run
+						// 	// delete build run
+						// 	// poll to see if they are deleted <-?
+					}
+
+				}
+				fmt.Println("Task run end: -----")
+
 			}
+			// Delete Buildruns
 		}
 
 	}
